9e9585a254a275f210b91e85b00b3466d195176e
iamzwzhong <zhiwzhong@gmail.com>
2021-12-13 00:23:23 -0800

    minor usage of jtest

---
+++ client/tests/TestConstants.js
@@ -0,0 +1,13 @@
+export default class TestConstants {
+    static noWinBoard = [
+        -1, -2, 0,
+        1, 1, 0, 
+        -1, -1, -1
+    ];
+
+    static winningBoard = [
+        'B', 1, 0,
+        1, 2, 1,
+        0, 0, 'B'
+    ]
+}
